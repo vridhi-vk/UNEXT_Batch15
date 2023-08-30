6a7cdcf51656d3c8cad6de6c657b462791bcaa64
J. Venkateswar Achary <86512619+jVenkateswarachary@users.noreply.github.com>
2023-08-30 07:37:56 +0000

    assignment 2 added

---
+++ Venkateswar/StudentDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEShellProgram
+{
+    internal class StudentDetails
+    {
+        private int rollno;
+        private string? name, address;//nullable string
+        private long phno;
+
+        public StudentDetails(int rollno, string name, string address, long phno)
+        {
+            Rollno = rollno;
+            Name = name;
+            Address = address;
+            Phno = phno;
+        }
+
+        public int Rollno { get => rollno; set => rollno = value; }
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public long Phno { get => phno; set => phno = value; }
+    }
+}
