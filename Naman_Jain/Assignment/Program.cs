d1cea86ea4e2c6b703f9b6c9640df5686c2d2e06
NamanJain1902 <1902ben10@gmail.com>
2023-08-30 11:26:47 +0000

    assignment1

---
+++ Naman_Jain/Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Assignment1;
+
+
+/*
+Dog bulldog = new Dog("bulldog", 5);
+bulldog.MakeSound();
+
+
+Cat cuttu = new Cat("cuttu", 2);
+cuttu.MakeSound();
+*/
+
+Rectangle rectangle = new Rectangle(123);
+rectangle.GetInfo();
+
+Circle circle = new Circle(2);   
+circle.GetInfo();
