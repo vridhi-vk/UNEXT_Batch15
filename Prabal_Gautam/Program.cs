ac958faee64e95f8db7f07633061b469af3aed7b
Prabal Gautam <55079251+prabalgautam01@users.noreply.github.com>
2023-08-30 05:10:44 +0000

    Create Program.cs

---
+++ Prabal_Gautam/Program.cs
@@ -0,0 +1,17 @@
+using SimplePrograms;
+
+//default constructor
+//Calculations calculations = new Calculations();
+
+
+Console.WriteLine("Enter 2 numbers");
+double number1 = Convert.ToDouble(Console.ReadLine());
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+//parameterised constructor
+Calculations calculations = new
+                        Calculations(number1, number2);
+
+int ans = calculations.Addition();
+
+Console.WriteLine(ans);
