bd10849846e7aaf9a4f40251527446e63b1831a5
Vridhi <57508390+vridhi-vk@users.noreply.github.com>
2023-08-30 11:17:53 +0000

    Add files via upload

---
+++ vridhi/Assignment1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using AnimalPrograms;
+
+Animal a = new Animal("Birdie", 1);
+Bird b = new Bird("Birdie", 1);
+
+b.MakeSound();
+a.MakeSound();
+
