3b3bf59d185c52df7cffb96d0e54f75163e96b30
Sai Mahesh <76741307+saimaheshpb@users.noreply.github.com>
2023-08-30 11:30:06 +0000

    Add files via upload

---
+++ mahesh/assignment/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms.Assignment.Assignment_1
+{
+    internal class Bird: Animal
+    {
+        public Bird(string name, int age) : base(name, age)
+        {
+        }
+
+        public new void MakeSound()
+        {
+            Console.WriteLine("bird says TWEET TWEET");
+        }
+    }
+}
---
+++ mahesh/assignment/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms.Assignment.Assignment_2
+{
+    internal class Circle: Shape
+    {
+        private double radius;
+
+        public double Radius { get => radius; set => radius = value; }
+
+        public Circle(string name, double radius): base(name)
+        {
+            Radius = radius;
+        }
+
+        public new void GetInfo()
+        {
+            Console.WriteLine("This is a circle. Radius is " + Radius + "\nArea is " + CalculateArea());
+        }
+
+        public double CalculateArea()
+        {
+            return double.Pi * Radius * Radius;
+        }
+    }
+}
