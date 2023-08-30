ca8fb910cb1b10d36cba237ba50873a779b20a49
NamanJain1902 <1902ben10@gmail.com>
2023-08-30 05:41:38 +0000

    feat: oops-with-c#-basics

---
+++ Naman_Jain/C#/Day2/Program.cs
@@ -0,0 +1,17 @@
+﻿using SamplePrograms;
+
+
+int rollno = Convert.ToInt32(Console.ReadLine());
+string? name = Console.ReadLine();
+string? address = Console.ReadLine();
+long phno = Convert.ToInt64(Console.ReadLine());
+
+// parameterized constructor
+StudentDetails studentDetails = new StudentDetails(rollno, name, address, phno);
+
+
+Console.WriteLine(studentDetails.Rollno);
+Console.WriteLine(studentDetails.Name);
+Console.WriteLine(studentDetails.Address);
+Console.WriteLine(studentDetails.Phno);
+
---
+++ Naman_Jain/C#/Day2/StudentDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplePrograms
+{
+    internal class StudentDetails
+    {
+        private int rollno;
+        private string? name, address; // nullable string
+        private long phno;
+
+        public StudentDetails(int rollno, string name, string address, long phno)
+        {
+            Rollno = rollno;
+            Name = name;
+            Address = address;
+            Phno = phno;
+        }
+
+        // property, getters & setters
+        public int Rollno { get => rollno; set => rollno = value; }
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public long Phno { get => phno; set => phno = value; }
+    }
+}
