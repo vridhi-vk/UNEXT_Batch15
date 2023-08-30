8a0332e36e514f8c42a02982f0b9c637173c04e1
Priya Aggarwal <55858346+Priya2123@users.noreply.github.com>
2023-08-30 10:38:48 +0000

    Create IBikes.cs

---
+++ Priya_Aggarwal/IBikes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms
+{
+    internal interface IBikes
+    {
+        // Abstract method - has no implementation
+        public void MakeSound(string sound);
+    }
+}
