981a6011c856dc7954b0bbfde815ae312018d26a
PUSHKAR BHAT <87753576+pushbhat@users.noreply.github.com>
2023-08-30 05:20:48 +0000

    Add files via upload

---
+++ Pushkar/StudentMarks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplePrograms
+{
+    internal class StudentMarks :StudentDetails
+    {
+        private int mark1,mark2,mark3;
+
+        public StudentMarks(int rollno, string name, string address, long phno, 
+            int mark1, int mark2, int mark3) 
+            : base(rollno, name, address, phno)
+        {
+            Mark1 = mark1;
+            Mark2 = mark2;
+            Mark3 = mark3;
+
+        }
+
+        public int Mark1 { get => mark1; set => mark1 = value; }
+        public int Mark2 { get => mark2; set => mark2 = value; }
+        public int Mark3 { get => mark3; set => mark3 = value; }
+
+        //Methods
+        public void CalculateScores()
+        {
+            int total = Mark1 + Mark2+ Mark3;
+            double avg = total / 3;
+            Console.WriteLine(total + " "+ avg);
+        }
+    }
+
+    
+}
