7f4bf77070243cedc1f470dd2951a80d104159ce
K-Rage <sbhadauria_be19@thapar.edu>
2023-08-31 03:58:51 +0000

    Added exceptions folder

---
+++ Shaurya_Pratap_Singh/Exceptions/MyExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    internal class MyExceptions
+    {
+        public static List<string> ExceptionMessages { get; } =
+        new List<string>
+            {
+                "Account No not Valid",
+                "Low Balance",
+                "Amount should be greater than zero"
+            };
+
+    }
+}
+
