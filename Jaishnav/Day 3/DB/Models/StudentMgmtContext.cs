d29aacc4f06f326cef2490d34a2923a248730154
Jaishnav <jaishnavgopi@gmail.com>
2023-08-31 11:19:02 +0000

    code push 3

---
+++ Jaishnav/Day 3/DB/Models/StudentMgmtContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirst.Models;
+
+public partial class StudentMgmtContext : DbContext
+{
+    public StudentMgmtContext()
+    {
+    }
+
+    public StudentMgmtContext(DbContextOptions<StudentMgmtContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Dummy> Dummies { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost; Database=StudentMgmt; Username=postgres; Password=admin");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dummy>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("dummy");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(10)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Rollno).HasName("student_pkey");
+
+            entity.ToTable("student");
+
+            entity.Property(e => e.Rollno)
+                .ValueGeneratedNever()
+                .HasColumnName("rollno");
+            entity.Property(e => e.City)
+                .HasMaxLength(20)
+                .HasColumnName("city");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Jaishnav/Day 3/DB/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using DBFirst.Models;
+
+CRUDOperations cRUDOperations = new CRUDOperations();
+//cRUDOperations.InsertRecords();
+//cRUDOperations.ReadData("HSR");
+//cRUDOperations.UpdateRecords();
+cRUDOperations.DeleteRecords();
