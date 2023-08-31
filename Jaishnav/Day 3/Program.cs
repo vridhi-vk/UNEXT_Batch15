59e4677cb8dcd43fd76b52da6d5e7fd6f80f1ea0
Jaishnav <jaishnavgopi@gmail.com>
2023-08-31 10:07:37 +0000

    code push 2

---
+++ Jaishnav/Day 3/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using SEShellPrograms;
+
+/*
+// Array Manipulation
+ArrayManipulations arrayManipulations = new ArrayManipulations();
+arrayManipulations.disp1d();
+arrayManipulations.disp2d();
+arrayManipulations.dispJA();
+*/
+
+/*
+// File Handling
+FileHandling fileHandling = new FileHandling();
+fileHandling.CreateFile();
+fileHandling.WriteData();
+fileHandling.ReadData();
+fileHandling.FileProperties();
+fileHandling.CopyMoveFile();
+fileHandling.DeleteFile();
+*/
+
+/*
+// Non Generic Collections
+NonGenericCollections nonGenericCollections = new NonGenericCollections();
+nonGenericCollections.ArrayListElements();
+nonGenericCollections.StackElements();
+nonGenericCollections.QueueElements();
+nonGenericCollections.HashElements();
+nonGenericCollections.SortedListElements();
+*/
+
+/*
+// Generic Collections
+GenericCollections genericCollections = new GenericCollections();
+genericCollections.ListElements();
+genericCollections.StackElements();
+genericCollections.QueueElements();
+genericCollections.HashElements();
+genericCollections.SortedListElements();
+*/
