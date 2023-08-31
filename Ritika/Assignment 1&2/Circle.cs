a49343e554bd8006fa45333c1bb5f0b27a8abda1
Singh <Ritika.Singh@shell.com>
2023-08-31 10:40:54 +0530

    ..;

---
+++ Ritika/Assignment 1&2/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Circle
+    {
+        private int radius;
+
+        public Circle(int radius) { Radius = radius; }
+
+        public int Radius { get => radius; set => radius = value; }
+
+        public new void GetInfo()
+        {
+            Console.WriteLine("Circle's radius is " + Radius);
+        }
+    }
+}
